b6ecde745f6af51fc5b440f71c48ef715e764191
Kyle <kylepcole@gmail.com>
2020-04-08 18:30:57 -0400

    begin of sass

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ProjecItem(props)  {
 	console.log("ProjectItem", props.project)
 	return(
-			<div >
+			<div className="project">
 				<img src={ props.project.thumbnail } />
 				<h2>{props.project.name}</h2>
 			</div>
