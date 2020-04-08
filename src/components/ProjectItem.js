5dbf84b64508f017a1c25bdf495bd0d832bf432c
Kyle <kylepcole@gmail.com>
2020-04-08 19:17:54 -0400

    saas is finally working

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 function ProjecItem(props)  {
-	console.log("ProjectItem", props.project)
+	
 	return(
 			<div className="project">
-				<img src={ props.project.thumbnail } />
+				{ <img src={ props.project.thumbnail} onError={event => event.target.src = "https://via.placeholder.com/300"} /> }
+				{ /*<div style={{ background: `url(${props.project.thumbnail ? props.project.thumbnail : "https://via.placeholder.com/300"}) 50% 50% no-repeat` }}></div>*/ }
 				<h2>{props.project.name}</h2>
 			</div>
 	)
--- src/components/SkillItem.js
+++ src/components/SkillItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function SkillItem(props)  {
+function SkillItem (props) {
+	//https://www.fillmurray.com/128/128
 	return(
-			<div className="col">
-				<img src={`/icons/skills/${props.skill.name.toLowerCase()}-128.png`} />
-				<p>{props.skill.name}</p>
-			</div>
+		<div className="col">
+			<img src={`/icons/skills/${props.skill.name.toLowerCase()}-128.png`} onError={event => event.target.src = "https://via.placeholder.com/128"} />
+			<p>{props.skill.name}</p>
+		</div>
 	)
 }
 
