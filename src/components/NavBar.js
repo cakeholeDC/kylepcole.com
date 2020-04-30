56c6e430fc375c6b06687f18f781ee84a4c126b5
Kyle <kylepcole@gmail.com>
2020-04-30 11:48:23 -0400

    navbar is mobile friendly

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import { device, size } from '../device';
+import ResponsiveMenu from 'react-responsive-navbar';
+// import { FaBars, FaClose } from 'react-icons/fa';
 
 const Navbar = styled.nav`
 	display: flex;
@@ -17,20 +20,44 @@ const Navbar = styled.nav`
 			color: lightgray;
 		}
 	}
+	
 
+	@media only screen and ${device.mobileL} {
+		flex-direction: column;
+		background-color: white;
+	    padding: 1rem;
+	    border: 1px solid black;
+	    text-align: right;
+	    clear: right;
 
+	}
+`
+
+const MobileMenuButton = styled.img`
+	max-height: 2rem;
+	max-width: 2rem;
+	float:right;
+	margin-bottom: .5rem;
 `
 
 export default class NavBar extends React.Component {
 	render(){
 		return(
-			<Navbar>
-				<a href="/">About</a>
-				<a href="/projects">Projects</a>
-				<a href="/blog">Blog</a>
-				<a href="/resume">Resume</a>
-				<a href="/contact">Contact</a>
-			</Navbar>
+			<ResponsiveMenu
+		        menuOpenButton={<MobileMenuButton src="/icons/navigation/bars-solid.svg" />}
+	            menuCloseButton={<MobileMenuButton src="/icons/navigation/times-solid.svg" />}
+		        changeMenuOn={ size.mobileL }
+		        largeMenuClassName="large-menu"
+		        smallMenuClassName="mobile-menu"
+		        menu={<Navbar>
+					<a href="/">About</a>
+					<a href="/projects">Projects</a>
+					<a href="/blog">Blog</a>
+					<a href="/resume">Resume</a>
+					<a href="/contact">Contact</a>
+					</Navbar>
+				}
+			/>
 		)
 	}
 }
