bd23de84a0b98c691eb9a6514a27dda43c07899e
Kyle <kylepcole@gmail.com>
2020-05-04 16:53:32 -0400

    reinstall packages

---
+++ src/data/FormMessages.js
@@ -0,0 +1,19 @@
+const FormMessages = [
+	{
+		name: "Fred Flintstone",
+		email: "fred@bedrock.com",
+		message: "Hello, my wife Wilma and her friend Betty Rubble have decided to start a catering business. We need a website for online ordering, can you help us?"
+	},
+	{
+		name: "Luke Skywalker",
+		email: "luke@jedi.net",
+		message: "If I don’t make it back, you’re the only hope for the Alliance."
+	},
+	{
+		name: "Bill Murray",
+		email: "me@billmurray.com",
+		message: "The more relaxed you are, the better you are at everything--the better you are with your loved ones, the better you are with enemies, the better you are at your job, the better you are with yourself."
+	}
+]
+
+export default FormMessages
