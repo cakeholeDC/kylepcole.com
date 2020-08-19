1769cddd89a05c194efea17caae18427a4f8f764
Kyle <kylepcole@gmail.com>
2020-08-19 12:53:26 -0400

    updated skills and dependencies

--- src/data/skillList.js
+++ src/data/skillList.js
@@ -1,5 +1,5 @@
 const skillList = [
-	// FRONT END
+	// ##### FRONT END ####
 	{ name: "HTML5", icon: "html5.svg" },
 	{ name: "CSS3", icon: "css3.svg" },
 	{ name: "SASS/SCSS", icon: "sass.svg" },
@@ -10,17 +10,19 @@ const skillList = [
 	// { name: "React Native", icon: "react.svg" },
 	{ name: "Angular", icon: "angular.svg" },
 	{ name: "VueJS", icon: "vue.svg" },
+	{ name: "Next.js", icon: "Nextjs.svg" },
 	{ name: "Php", icon: "php.svg" },
 	{ name: "TypeScript", icon: "typescript.svg" },
 	{ name: "PostScript", icon: "postscript2.svg" },
-	// BACK END
+	// #### BACK END ####
 	{ name: "Python", icon: "python-icon.svg" },
+	// { name: "Flask", icon: "flask.svg" },
 	{ name: "Ruby", icon: "ruby.svg" },
 	{ name: "Rails", icon: "rails.svg" },
 	{ name: "PostgreSQL", icon: "postgres.svg" },
 	// { name: "SQL", icon: "postgres.svg" },
 	{ name: "Node.js", icon: "nodejs.svg" },
-	// DEV OPS
+	// #### DEV OPS ####
 	{ name: "npm", icon: "npm-logo-red.svg" },
 	{ name: "GIT", icon: "git.svg" },
 	{ name: "Docker", icon: "docker-moby-logo.png" },
@@ -29,7 +31,7 @@ const skillList = [
 	{ name: "Amazon Web Services", icon: "aws.svg" },
 	{ name: "Google Cloud Platform", icon: "Google_Cloud_Icon.svg" },
 	{ name: "Heroku", icon: "heroku.svg" },
-	// SOFTWARE
+	// #### SOFTWARE ####
 	{ name: "Creative Cloud", icon: "creativeCloud.svg" },
 	{ name: "Photoshop", icon: "photoshop-cc.svg" },
 	{ name: "Illustrator", icon: "adobe-illustrator-cc.svg" },
